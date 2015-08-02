8bba8ee0364fc520bbd35532e2eb7c69de928836
Alban Montaigu <alban.montaigu@gmail.com>
2015-08-02 14:20:44 +0200

    Manage upgrade message

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,6 +12,9 @@ tar -zcf /var/backup/dokuwiki/dokuwiki-v$(date '+%y%m%d%H%M%S').tar.gz /var/www
 echo >&2 "Installing or upgrading dokuwiki in $(pwd) - copying now..."
 tar cf - --one-file-system -C /usr/src/dokuwiki . | tar xf -
 grep -Ev '^($|#)' data/deleted.files | xargs -n 1 rm -vf
+# To disable wrong update messages
+# @see https://www.dokuwiki.org/update_check
+rm -f data/cache/messages.txt 
 chown -R nginx:nginx ./
 echo >&2 "Complete! Dokuwiki has been successfully installed / upgraded to $(pwd)"
 
